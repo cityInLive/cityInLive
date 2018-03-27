302280587c9bbe59f3d7d637bcea0a02924797f1
Valentin Pelloin <valentinpelloin@gmail.com>
2018-03-27 13:40:46 +0200

    avant droit

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -10,4 +10,17 @@ angular.module('liveCityApp').controller('mainCtrl', ['$scope', function($scope)
 		$scope.text = 'Text: ' + city.name;
 	};
 
+
 }]);
+
+function initMap() {
+	var uluru = {lat: 47.7510112, lng: 2.3148873};
+	var map = new google.maps.Map(document.getElementById('map'), {
+		zoom: 6,
+		center: uluru
+	});
+	var marker = new google.maps.Marker({
+		position: uluru,
+		map: map
+	});
+}
