95fc9f7c027810c7197c1df7772882cfa7ab6bc6
Valentin Pelloin <valentinpelloin@gmail.com>
2018-03-27 18:07:19 +0200

    ça marche

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -2,25 +2,264 @@
 
 
 angular.module('liveCityApp').controller('mainCtrl', ['$scope', function($scope) {
-	$scope.text = 'hello';
-
-	//$scope.city.name = 'test';
+	$scope.text = '';
 
 	$scope.update = function(city) {
-		$scope.text = 'Text: ' + city.name;
+		$scope.text = 'Rechercher' + city.name;
+		city.name = city.name.replace(/\b\w/g, l => l.toUpperCase());
+	};
+
+	$scope.visit = function(city) {
+		$scope.text = 'Visiter' + city.name;
+	};
+
+	$scope.allowVisit = function(city) {
+		if (city === undefined) {
+			return false;
+		}
+		$scope.update(city);
+		return city.name.length > 4;
+	};
+
+
+	$scope.havePanel = function() {
+		return false;
 	};
 
 
 }]);
 
 function initMap() {
-	var uluru = {lat: 47.7510112, lng: 2.3148873};
+	var france = {lat: 47.7510112, lng: 2.3148873};
+	var paris  = {lat: 48.859489,  lng: 2.320582};
 	var map = new google.maps.Map(document.getElementById('map'), {
 		zoom: 6,
-		center: uluru
+		disableDefaultUI: true,
+		styles: [
+			{
+				"elementType": "geometry",
+				"stylers": [
+					{
+						"color": "#f5f5f5"
+					}
+				]
+			},
+			{
+				"elementType": "labels.icon",
+				"stylers": [
+					{
+						"visibility": "off"
+					}
+				]
+			},
+			{
+				"elementType": "labels.text.fill",
+				"stylers": [
+					{
+						"color": "#616161"
+					}
+				]
+			},
+			{
+				"elementType": "labels.text.stroke",
+				"stylers": [
+					{
+						"color": "#f5f5f5"
+					}
+				]
+			},
+			{
+				"featureType": "administrative.land_parcel",
+				"elementType": "labels",
+				"stylers": [
+					{
+						"visibility": "off"
+					}
+				]
+			},
+			{
+				"featureType": "administrative.land_parcel",
+				"elementType": "labels.text.fill",
+				"stylers": [
+					{
+						"color": "#bdbdbd"
+					}
+				]
+			},
+			{
+				"featureType": "poi",
+				"elementType": "geometry",
+				"stylers": [
+					{
+						"color": "#eeeeee"
+					}
+				]
+			},
+			{
+				"featureType": "poi",
+				"elementType": "labels.text",
+				"stylers": [
+					{
+						"visibility": "off"
+					}
+				]
+			},
+			{
+				"featureType": "poi",
+				"elementType": "labels.text.fill",
+				"stylers": [
+					{
+						"color": "#757575"
+					}
+				]
+			},
+			{
+				"featureType": "poi.medical",
+				"stylers": [
+					{
+						"visibility": "on"
+					}
+				]
+			},
+			{
+				"featureType": "poi.park",
+				"elementType": "geometry",
+				"stylers": [
+					{
+						"color": "#e5e5e5"
+					}
+				]
+			},
+			{
+				"featureType": "poi.park",
+				"elementType": "labels.text.fill",
+				"stylers": [
+					{
+						"color": "#9e9e9e"
+					}
+				]
+			},
+			{
+				"featureType": "road",
+				"elementType": "geometry",
+				"stylers": [
+					{
+						"color": "#ffffff"
+					}
+				]
+			},
+			{
+				"featureType": "road.arterial",
+				"elementType": "labels",
+				"stylers": [
+					{
+						"visibility": "off"
+					}
+				]
+			},
+			{
+				"featureType": "road.arterial",
+				"elementType": "labels.text.fill",
+				"stylers": [
+					{
+						"color": "#757575"
+					}
+				]
+			},
+			{
+				"featureType": "road.highway",
+				"elementType": "geometry",
+				"stylers": [
+					{
+						"color": "#dadada"
+					}
+				]
+			},
+			{
+				"featureType": "road.highway",
+				"elementType": "labels",
+				"stylers": [
+					{
+						"visibility": "off"
+					}
+				]
+			},
+			{
+				"featureType": "road.highway",
+				"elementType": "labels.text.fill",
+				"stylers": [
+					{
+						"color": "#616161"
+					}
+				]
+			},
+			{
+				"featureType": "road.local",
+				"stylers": [
+					{
+						"visibility": "off"
+					}
+				]
+			},
+			{
+				"featureType": "road.local",
+				"elementType": "labels",
+				"stylers": [
+					{
+						"visibility": "off"
+					}
+				]
+			},
+			{
+				"featureType": "road.local",
+				"elementType": "labels.text.fill",
+				"stylers": [
+					{
+						"color": "#9e9e9e"
+					}
+				]
+			},
+			{
+				"featureType": "transit.line",
+				"elementType": "geometry",
+				"stylers": [
+					{
+						"color": "#e5e5e5"
+					}
+				]
+			},
+			{
+				"featureType": "transit.station",
+				"elementType": "geometry",
+				"stylers": [
+					{
+						"color": "#eeeeee"
+					}
+				]
+			},
+			{
+				"featureType": "water",
+				"elementType": "geometry",
+				"stylers": [
+					{
+						"color": "#c9c9c9"
+					}
+				]
+			},
+			{
+				"featureType": "water",
+				"elementType": "labels.text.fill",
+				"stylers": [
+					{
+						"color": "#9e9e9e"
+					}
+				]
+			}
+		],
+		center: france
 	});
 	var marker = new google.maps.Marker({
-		position: uluru,
+		position: paris,
 		map: map
 	});
 }
