ae8b42dad5a69242831ba9957dccdd1b859be9fb
Valentin Pelloin <valentinpelloin@gmail.com>
2018-04-05 10:19:43 +0200

    panel start

--- src/app/panel/panel.component.ts
+++ src/app/panel/panel.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, NgZone } from '@angular/core';
+import { Component, OnInit, NgZone, HostListener } from '@angular/core';
 import { SimpleSmoothScrollService } from 'ng2-simple-smooth-scroll';
 import { SimpleSmoothScrollOption } from 'ng2-simple-smooth-scroll';
 
@@ -7,22 +7,42 @@ import { SimpleSmoothScrollOption } from 'ng2-simple-smooth-scroll';
 	templateUrl: './panel.component.html',
 	styleUrls: ['./panel.component.css']
 })
+
 export class PanelComponent implements OnInit {
 
 	public cityName:  string;
 	public lat:       number;
 	public lng:       number;
 	public showPanel: boolean;
+	public fixedBar:  boolean;
 
 	public constructor(
 		private smooth: SimpleSmoothScrollService,
 		private zone: NgZone
 		) {
-
 	}
 
 	public ngOnInit() {
 		this.showPanel = false;
+		this.fixedBar  = false;
+		this.debug();
+	}
+
+	@HostListener('window:scroll', [])
+	public onWindowScroll() {
+		let panelAreaPos = document.getElementById('module-area').getBoundingClientRect();
+		if(panelAreaPos.top < 40) {
+			if(!this.fixedBar)
+				this.fixedBar = true;
+		}
+		else {
+			if(this.fixedBar)
+				this.fixedBar = false;
+		}
+	}
+
+	public debug() {
+		this.showCity("Paris", 48.86666, 2.333333);
 	}
 
 	public showCity(cityName: string, lat: number, lng: number) {
@@ -36,7 +56,8 @@ export class PanelComponent implements OnInit {
 	public scroll() {
 		window.location.hash = "#SimpleSmoothScroll";
 		this.smooth.smoothScrollToAnchor();
-		window.location.hash = "";
+		//window.location.hash = "";
+		//document.getElementById("SimpleSmoothScroll").scrollIntoView();
 	}
 
 }
